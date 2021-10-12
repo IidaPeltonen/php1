40ef76004b234436cd03b605ce785cc775e42fb4
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-12 12:07:20 +0300

    ok

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
 print 'Hello from PHP server';
+
+header('Content-Type: application/json');
+
+$data = array (
+    'message' => 'Hello from PHP server',
+    'origin' => $_SERVER['HTTP_ORIGIN']
+);
+$json = json_encode($data,JSON_PRETTY_PRINT);
+print $json;
